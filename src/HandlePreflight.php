635976ccd4ed6e364cfe7805a4283e21e364a7cb
Ludmil Simeonov <lsimeonov@despark.com>
2017-09-11 14:13:56 +0300

    Remove not needed checks in pre flight request checker

--- src/HandlePreflight.php
+++ src/HandlePreflight.php
@@ -28,49 +28,9 @@ class HandlePreflight
     public function handle($request, Closure $next)
     {
         if ($this->cors->isPreflightRequest($request)) {
-            if (! $this->isLumen() && ! $this->hasMatchingCorsRoute($request)) {
-                return new Response('Not allowed.', 403);
-            }
-
             return $this->cors->handlePreflightRequest($request);
         }
 
         return $next($request);
     }
-
-    /**
-     * Verify the current OPTIONS request matches a CORS-enabled route. Only possible on Laravel (not Lumen)
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return boolean
-     */
-    private function hasMatchingCorsRoute($request)
-    {
-        // Check if CORS is added as a route middleware
-        $request = clone $request;
-        $request->setMethod($request->header('Access-Control-Request-Method'));
-
-        /** @var Router $router */
-        $router = app(Router::class);
-        try {
-            $route = $router->getRoutes()->match($request);
-        } catch (HttpException $e) {
-            return false;
-        }
-
-        // change of method name in laravel 5.3
-        if (method_exists($router, 'gatherRouteMiddleware')) {
-            $middleware = $router->gatherRouteMiddleware($route);
-        } else {
-            $middleware = $router->gatherRouteMiddlewares($route);
-        }
-
-        return in_array(HandleCors::class, $middleware);
-    }
-
-    protected function isLumen()
-    {
-        return str_contains(app()->version(), 'Lumen');
-    }
-
 }
